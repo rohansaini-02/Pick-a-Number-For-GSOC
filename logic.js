e47651730e9050a0f43ec3224366b40614e0d912
rohansaini-02 <rohansaini0916@gmail.com>
2024-11-21 15:22:28 +0530

    Add

---
+++ logic.js
@@ -0,0 +1,41 @@
+let cnum = Math.floor(Math.random() * 100) + 1;
+let attempt = 0;
+let attemptData = document.getElementById("Attempt");
+let userinp = document.getElementById("inp");
+let subBtn = document.getElementById("submit");
+let resBtn = document.getElementById("resBtn");
+let msg = document.getElementById("msg");
+
+function check() {
+    let usernum = parseInt(userinp.value);
+    if (cnum === usernum) {
+        msg.innerHTML = "🎉Congratulations, you have guessed the number!🎉";
+        msg.style.color = "green";
+        resBtn.style.display = "block";
+        setTimeout(() => {
+            msg.innerHTML = ""; // Clear message after 5 seconds
+        }, 10000); // 5000 milliseconds (5 seconds)
+    } else if (cnum < usernum) {
+        msg.innerHTML = "Too high! Try again 😵‍💫";
+        msg.style.color = "red";
+    } else {
+        msg.innerHTML = "Too low! Try again 😶‍🌫️";
+        msg.style.color = "red";
+    }
+    attempt++;
+    attemptData.innerHTML = attempt;
+    setTimeout(() => {
+        userinp.value = "";
+    }, 500); // 1000 milliseconds (1 second)
+}
+
+function restart() {
+    cnum = Math.floor(Math.random() * 100) + 1;
+    attempt = 0;
+    msg.innerHTML = "";
+    attemptData.innerHTML = attempt;
+    resBtn.style.display = "none";
+}
+
+subBtn.addEventListener("click", check);
+resBtn.addEventListener("click", restart);
